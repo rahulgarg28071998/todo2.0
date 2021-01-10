5e9016a16f2c9bd3bb9fa7c3c80fb1df72fbe247
rahulgarg28071998 <rgarg8438@gmail.com>
2021-01-10 18:23:25 +0530

    port process added

--- app.js
+++ app.js
@@ -25,5 +25,9 @@ mongoose.connect(
     process.env.DB_CONNECTION,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log("connected to db"));
-app.listen(3000);
+
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 app.use(express.static(__dirname + '/public'));
